d84c55fd5694f612440aa822cb945c1551c6c7f9
Daniel Strong <danielstrong@mac.com>
2017-06-07 09:19:54 -0800

    got more passing

--- my-fill-in-the-blanks.py
+++ my-fill-in-the-blanks.py
@@ -8,20 +8,39 @@ answers = [['Python','string','list','array'],['dictionary', 'data', '2', '3'],[
 
 sentence_with_blanks = ""
 
-def level_selector(level):
+def level_selector():
+    """User selected level is returned to user, along with the appropriate sentence
+
+    Args:
+        None
+
+    Returns:
+        string: Returns numeric position of level within sentences array, otherwise calls itself to start over
+
+    """
+    level = int(raw_input("Please select a level to begin at: (1) Easy (2) Medium or (3) Hard"))
     if level == 1:
         print 'Level is: Easy'
+        return level - 1
     elif level == 2:
         print 'Level is: Medium'
+        return level - 1
     elif level == 3:
         print 'Level is: Hard'
-    elif level == 'Q':
-        break
+        return level - 1
     else:
-        return 'Please select a level to begin at: (1) Easy (2) Medium or (3) Hard. Type 'Q' to quit' 
-    print sentences[level - 1]
+        level_selector() # if inputs are not integers 1-3, start over
 
 def sentence_play(level):
+    """Allows user to enter selection and validates user response
+
+    Args:
+        level (int): level selected by user
+
+    Returns:
+        Does not return anything
+
+    """
     lvl = level
     i = 1
     while i < 4:
@@ -37,24 +56,60 @@ def sentence_play(level):
         if correct_answer(lvl, i, blank_input):
              # When player guesses correctly, new prompt shows with correct answer in the
              # previous blank and a new prompt for the next blank
-            display_sentence(lvl, i, blank_input):
-            print 'Nice work! You got that one. Now on to the next blank..."
+            display_filled_sentence(lvl, i, blank_input)
+            print "Nice work! You got that one. Now on to the next blank..."
             i += 1
         else:
             # When player guesses incorrectly, they are prompted to try again
             print 'Not quite. Try again.'
     print "Congrats! You got them all right!"
     play_game()
-        
+
 
 # Checks if a answer is correct
 def correct_answer(level, blank_number, answer):
+    """Validates user response against answers array
+
+    Args:
+        level (int): level selected by user
+        blank_number (int): number of blank within sentence
+        answer (string): answer provided by user
+
+    Returns:
+        boolean: Returns true if answer is correct
+
+    """
     if str(answer) == answers[level - 1][blank_number - 1]:
         return True
     else:
         return False
 
-def display_sentence(level, position, blank_input):
+def display_blank_sentence():
+    """Validates user response against answers array
+
+    Args:
+        level (int): level selected by user
+        blank_number (int): number of blank within sentence
+        answer (string): answer provided by user
+
+    Returns:
+        boolean: Returns true if answer is correct
+
+    """
+    print sentences[level - 1]
+
+def display_filled_sentence(level, position, blank_input):
+    """Displays the sentence as the user fills it in
+
+    Args:
+        level (int): level selected by user
+        position (int): position of blank within sentence
+        blank_input (string): user answer for blank
+
+    Returns:
+        Does not return anything
+
+    """
     sentence = sentences[level]
     sentence.split(" ")
     for word in sentence:
@@ -64,22 +119,27 @@ def display_sentence(level, position, blank_input):
         else:
             sentence = " ".join(sentence)
     print sentence
-            
-# prompt user and play through game
+
+
 def play_game():
+    """Prompts user and plays through game
+
+    Args:
+        None
+
+    Returns:
+        Does not return anything
+
+    """
     # Start game with a prompt to the user
     print "Welcome to the fill-in-the-blanks quiz"
-
     # Immediately after running the program, user is prompted to select a difficulty level
     # from easy / medium / hard
-    first_input = raw_input("Please select a level to begin at: (1) Easy (2) Medium or (3) Hard. Type 'Q' to quit")
+    # Once a level is selected, game displays a fill-in-the-blank and a prompt to fill
+    # in the first blank.
+    level_selector()
+    level = level_selector()
+    sentence_play(level)
+    play_game()
 
-    while first_input != 'Q':
-        # Once a level is selected, game displays a fill-in-the-blank and a prompt to fill
-        # in the first blank.
-        level_selector(first_input)
-        level = first_input
-        sentence_play(level)
-        play_game()
-        
 play_game()
