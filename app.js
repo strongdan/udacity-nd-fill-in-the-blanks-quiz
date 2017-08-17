0b8621abfa64696161ad72c96117bdf2738270ae
Dan Strong <danielstrong@mac.com>
2017-08-17 12:00:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -10,16 +10,10 @@ let sentencesWithAnswers = { // sentences and their corresponding answers, organ
   2: {'sentence': 'A _1_ is an associative array that can contain any Python _2_ type. The print() function and integer division are key differences between Python _3_ and _4_',
       'answers': ['dictionary', 'data', '2', '3']},
   3: {'sentence': 'Python supports _1_ programming with classes and multiple _2_. Python _3_ are defined by creating a function and inheriting from a _4_ eg. variable = Class(parameters)',
-      'answers': ['object-oriented', 'inheritance','objects','class']},
-}
+      'answers': ['object-oriented', 'inheritance','objects','class']}
+  }
 
 // Game has 3 or more levels and each level contains 4 or more blanks to fill in
-const sentences = [
-"_1_ is a programming language named for Monty Python's Flying Circus. A _2_ is an immutable list of characters surrounded by quotes. A _3_ is an unordered collection of items of any type while a _4_ is an ordered collection of a single type.",
-"A _1_ is an associative array that can contain any Python _2_ type. The print() function and integer division are key differences between Python _3_ and _4_",
-"Python supports _1_ programming with classes and multiple _2_. Python _3_ are defined by creating a function and inheriting from a _4_ eg. variable = Class(parameters)"]
-
-const answers = [['Python','string','list','array'],['dictionary', 'data', '2', '3'],['object-oriented', 'inheritance','objects','class']]
 
 const levelSelector = () => {
   // User selected level is printed for user, along with the appropriate sentence  
